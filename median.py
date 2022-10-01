3f698f72aca1796624fbdc2f12091a795c285031
Junhuai Hou <junhuai.hou@kcl.ac.uk>
2022-10-01 18:06:00 +0100

    homework

--- median.py
+++ median.py
@@ -1,6 +1,6 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+import statistics
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +9,4 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(statistics.median(ls))
